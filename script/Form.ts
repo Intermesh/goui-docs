9fbf5931039b68771871bf46d64923474646f089
Merijn Schering <mschering@intermesh.nl>
2025-01-03 12:15:29 +0100

    Added send

--- script/Form.ts
+++ script/Form.ts
@@ -5,8 +5,8 @@ import {
 	ComboBox,
 	combobox,
 	comp,
-	datefield,
-	fieldset,
+	datefield, displayfield,
+	fieldset, form,
 	h1, htmlfield,
 	img,
 	p,
@@ -29,73 +29,83 @@ export class Form extends Page {
 
 		this.items.add(
 
-			fieldset({
-				legend: "Example form"
+			form({},
+
+				fieldset({
+					legend: "Example form"
+					},
+
+					comp({cls: "group"},
+						textfield({
+							name: "firstName",
+							label: "First name"
+						}),
+
+						textfield({
+							name: "middleName",
+							label: "First name"
+						}),
+
+						textfield({
+							name: "lastName",
+							label: "Last name"
+						})
+					),
+
+					combobox({
+						dataSource: demoDataSource,
+						label: t("Contact"),
+						name: "contactId",
+						filterName: "name"
+
+					}),
+
+					combobox({
+						dataSource: demoDataSource,
+						label: t("Customer"),
+						name: "customerId",
+						filterName: "name"
+					}),
+				),
+
+				fieldset({},
+					datefield({
+						label: t("Starts at"),
+						name: "startsAt"
+					}),
+
+					datefield({
+						label: t("Due at"),
+						name: "dueAt"
+					}),
+
+					datefield({
+						label: t("Completed at"),
+						name: "completedAt"
+					}),
+				),
+
+				fieldset({
+
 				},
 
-				comp({cls: "group"},
-					textfield({
-						name: "firstName",
-						label: "First name"
-					}),
-
-					textfield({
-						name: "middleName",
-						label: "First name"
-					}),
-
-					textfield({
-						name: "lastName",
-						label: "Last name"
-					})
-				),
-
-				combobox({
-					dataSource: demoDataSource,
-					label: t("Contact"),
-					name: "contactId",
-					filterName: "name"
-
-				}),
-
-				combobox({
-					dataSource: demoDataSource,
-					label: t("Customer"),
-					name: "customerId",
-					filterName: "name"
-				}),
-			),
-
-			fieldset({},
-				datefield({
-					label: t("Starts at"),
-					name: "startsAt"
-				}),
-
-				datefield({
-					label: t("Due at"),
-					name: "dueAt"
-				}),
-
-				datefield({
-					label: t("Completed at"),
-					name: "completedAt"
-				}),
-			),
-
-			fieldset({
-
-			},
-
-				comp({cls: "group"},
-					btn({icon: "add"}),
-					htmlfield({
-						flex: 1
-					}),
-					btn({icon: "send"}),
-					)
+					comp({cls: "group"},
+						btn({icon: "add"}),
+						htmlfield({
+							flex: 1
+						}),
+						btn({icon: "send"}),
+					),
 
 				),
+
+				btn({
+					type: "submit",
+					text: "Submit",
+				})
+			),
+
+
 
 			p("Forms can handle complex object structures using Container and Array type fields. They don't submit in " +
 				"the traditional way but return a Javascript Object that can be sent using an XHR or fetch API request. " +
