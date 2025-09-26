3f939f8bc8b1a9adfe9cc69578559a88e1c52cd4
Merijn Schering <mschering@intermesh.nl>
2025-09-26 16:07:55 +0200

    Todo app

--- script/GetStarted.ts
+++ script/GetStarted.ts
@@ -8,13 +8,23 @@ export class GetStarted extends Page {
 		// this.sourceURL = './Layout.ts';
 
 		this.items.add(
-
 			p({
 				html:
-					"Start building efficient web apps now! Use our simple 'Hello World' boilerplate project. " +
+
+					"Start building efficient web apps now! To get you started you can checkout the components on this documentation" +
+					" site and we have some example or boilerplate repositories:" +
+					"<ul><li>Use our simple 'Hello World' boilerplate project. " +
 					"Go to the template repository: <br /><br /> " +
-					"<a href=\"https://github.com/Intermesh/goui-hello-world.git\">https://github.com/Intermesh/goui-hello-world.git</a>." +
-					"<br /><br />Clone it and run it."
+					"<a href=\"https://github.com/Intermesh/goui-hello-world\">https://github.com/Intermesh/goui-hello-world</a>" +
+
+					"<br /><br /></li>" +
+
+					"<li>A more extensive example is our todo app:<br /><br />" +
+					"<a href='https://github.com/Intermesh/goui-todo-app'>https://github.com/Intermesh/goui-todo-app</a><br /><br /></li>" +
+
+					"<li>You can also study the code of this site:<br /><br />" +
+					"<a href='https://github.com/Intermesh/goui-docs'>https://github.com/Intermesh/goui-docs</a><br /><br /></li>" +
+					"</ul>"
 			}),
 
 
